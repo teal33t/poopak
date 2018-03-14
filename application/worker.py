cc71775755ddea58e4f4ef8ae0b499c33e64a1e0
sparkmood <sumoneesma@gmail.com>
2018-03-14 14:41:30 +0330

    reconfig worker settings

--- application/worker.py
+++ application/worker.py
@@ -3,7 +3,17 @@ import os
 from rq import Worker, Queue, Connection
 from redis import Redis
 from urllib.parse import urlparse
-from .worker_config import redis_uri
+
+
+local_dev = False
+
+if local_dev:
+    mongodb_uri = "mongodb://%s:%s@localhost:27017/crawler" % ("admin", "54nn4n")
+    redis_uri = 'redis://localhost:6379'
+else:
+    mongodb_uri = "mongodb://%s:%s@mongodb:27017/crawler" % ("admin", "54nn4n")
+    redis_uri = 'redis://redis:6379'
+
 
 listen = ['default']
 redis_url = os.getenv('REDISTOGO_URL', redis_uri)
