5c74bac98d37fc4c6aa5bc436c9c16f5c96bc53a
saman <saman.2791@gmail.com>
2018-04-15 12:39:19 +0430

    Crawler extractor revised

--- application/web/config.py
+++ application/web/config.py
@@ -8,6 +8,6 @@ if local_dev:
     mongodb_uri = "mongodb://localhost:27017"
 else:
     redis_uri = 'redis://redis:6379'
-    mongodb_uri = "mongodb://%s:%s@mongodb:27017/crawler" % ("admin", "54nn4n")
+    mongodb_uri = "mongodb://%s:%s@mongodb:27017/crawler" % ("admin", "123qwe")
 
 seed_upload_dir = "/app/seeds/"
--- application/web/helper.py
+++ application/web/helper.py
@@ -5,7 +5,7 @@ def extract_onions(s):
     print(s)
     for m in re.finditer(r'(?:https?://)?(?:www)?(\S*?\.onion)\b', s, re.M | re.IGNORECASE):
         url = str(m.group(0))
-        url = url[len(url) - 22:]
+        # url = url[len(url) - 22:]
         # if "'" in url:
         #     url = url[url.index("'")+1:]
         result.append(url)
