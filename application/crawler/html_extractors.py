9370cc65d3698048e06ca90eea7418692cd111ca
saman <saman.2791@gmail.com>
2018-04-29 12:12:02 +0430

    Email detection added.

--- application/crawler/html_extractors.py
+++ application/crawler/html_extractors.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urlunparse
 
+import re
 
 
 class Extractor:
@@ -29,6 +30,7 @@ class Extractor:
                 in_scope = True
                 href = urlunparse((parsed_url.scheme, parsed_url.netloc, parsed_href.path, '', parsed_href.query, ''))
             if href.startswith("#"):
+                in_scope = True
                 href = urlunparse((parsed_url.scheme, parsed_url.netloc, parsed_href.path, '', parsed_href.query, ''))
             _urls.append({'url': href, 'is_onion': is_onion, 'in_scope': in_scope})
         return _urls
@@ -43,5 +45,40 @@ class Extractor:
         try:
             return self.soup.title.get_text()
         except:
-            pass
+            return None
+
+
+    # match email addresses from body
+    def get_emails(self):
+        try:
+            match = re.findall(r'[\w\.-]+@[\w\.-]+', self.html)
+            return match
+        except:
+            return None
+
+
+    # match bitcoin addresses from body
+    def get_bitcoin_addrs(self):
+        try:
+            _find = re.findall('[13][a-km-zA-HJ-NP-Z1-9]{25,34}', self.get_body())
+            return _find
+        except:
+            return None
+
+    # match eth addresses from body
+    def get_eth_addrs(self):
+        try:
+            _find = re.findall('0x[a-fA-F0-9]{40}', self.text)
+            return _find
+        except:
+            return None
+
+    # match monero addresses from body
+    def get_monero_addrs(self):
+        try:
+            _find = re.findall('4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}', self.get_body())
+            return _find
+        except:
+            return None
+
 
